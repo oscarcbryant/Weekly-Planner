3c0133d11e796aa221c3e2b5eff826abfea75999
Oscar Bryant <oscarcbryant@gmail.com>
2022-01-10 22:08:32 +1100

    updating html and js code

--- script.js
+++ script.js
@@ -1,3 +1,28 @@
 var todaysDate = moment().format("dddd MMMM Do");
 $("#currentDay").text(todaysDate);
 
+var currentHour = moment().hour();
+var timeBlock = $(".time-block");
+var hourSection = $(".hour-section");
+
+
+//timeBlock.css('background-color', 'green');
+currentHour = 12;
+hourSection.each(function() {
+var hour = $(this).attr('data-hour');
+
+if (hour < currentHour) {
+  
+    $(this).css('background-color', 'red');
+}
+
+else {
+    $(this).css('background-color', 'green');
+}
+})
+
+
+
+
+//localStorage.text("textarea");
+
