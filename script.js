b2a9e5f74f34b5440412b4812ae06cfb8fd31073
Oscar Bryant <oscarcbryant@gmail.com>
2022-01-11 13:14:27 +1100

    adding local storage component to code

--- script.js
+++ script.js
@@ -4,22 +4,46 @@ $("#currentDay").text(todaysDate);
 var currentHour = moment().hour();
 var timeBlock = $(".time-block");
 var hourSection = $(".hour-section");
+var taskInput = $(".hour-section");
+var saveButton = $(".saveBtn")
 
 
 //timeBlock.css('background-color', 'green');
-currentHour = 12;
+//currentHour = 13;
 hourSection.each(function() {
 var hour = $(this).attr('data-hour');
 
 if (hour < currentHour) {
   
-    $(this).css('background-color', 'red');
+    $(this).css('background-color', 'grey');
+}
+
+else if (hour > currentHour) {
+
+    $(this).css('background-color', 'green');
 }
 
 else {
-    $(this).css('background-color', 'green');
+    $(this).css('background-color', 'red');
 }
 })
+
+
+
+saveButton.on('click', function(event) {
+
+    event.preventDefault();
+
+var textarea = $(this).prev();
+var task = textarea.val();
+
+localStorage.setItem("hour" + textarea.attr('data-hour'), task);
+localStorage.getItem("hour");
+})
+
+
+
+
 
 
 
